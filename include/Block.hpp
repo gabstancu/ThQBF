b7fd539bd36679c9926744d40fd4f0551b5aa741
Gabriela Stancu <87585067+gabstancu@users.noreply.github.com>
2025-04-21 20:44:24 +0300

    Block.hpp done (temp.)

--- include/Block.hpp
+++ include/Block.hpp
@@ -11,11 +11,11 @@ class Block
         int blockID;
         size_t size;
         bool available;
-        int availableVars;
+        int available_variables;
 
         std::vector<int> variables;
         std::vector<int> state;
-        std::vector<int> level;
+        std::vector<int> decision_level;
         
     public:
         Block (char type, 
@@ -25,11 +25,40 @@ class Block
                blockID(blockID),
                size(variables.size()),
                available(true),
-               availableVars(variables.size()),
+               available_variables(variables.size()),
                variables(variables)
         {
             /* ... */
         }
+
+        /* Read only access */
+        char type() const { return type; }
+        int blockID() const { return blockID; }
+        size_t size() const { return size; }
+        bool is_available() const { return available; }
+        int get_available_variables() const { return available_variables; }
+        const std::vector<int>& get_variables() const { return variables; }
+        const std::vector<int>& get_state() const { return state; }
+        const std::vector<int>& get_decision_level() const { return decision_level; }
+
+
+        /* mutators (during search) */
+        void set_type(char t) { type = t; }
+        void set_blockID(int ID) { blockID = ID; }
+        void set_size(size_t s) { size = s; }
+        void set_availability(bool status) { available = status; }
+        void set_available_variables(int available_vars) { available_variables = available_vars; }
+
+        std::vector<int>& get_variables() { return variables; }
+        std::vector<int>& get_state() { return state; }
+        std::vector<int>& get_decision_level() { return decision_level; }
+
+        /* other */
+        bool is_existential() const { return type == qbf::EXISTENTIAL; }
+        bool is_universal() const { return type == qbf::UNIVERSAL; }
+
+        /* extend... */
+        void remove_variable();
 };
 
 #endif // BLOCK_HPP
